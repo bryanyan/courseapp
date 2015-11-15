bc0f584bf39f6835089b12e32f3365c1f0f4feef
Bryan Yan <bryanyan@andrew.cmu.edu>
2015-11-14 22:19:06 -0500

    template modules, filters

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 
+import cmu_course_api
+
+#Semester must be one of S (spring), M1 (Summer 1), M2 (Summer 2), or F (Fall).
+semester = 'S'
+# CMU AndrewID
+username = 'bryanyan'
+# Password 
+password = '*********'
+# Python Dictionary 
+# data = cmu_course_api.get_course_data(semester, username, password)
+
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
 
@@ -19,6 +30,30 @@ class Application(tornado.web.Application):
 class IndexHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render('index.html')
+
+class MCSHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.render('mcs.html')
+
+class SCSHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.render('scs.html')
+
+class DCHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.render('dc.html')
+
+class CITHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.render('cit.html')
+
+class CFAHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.render('cfa.html')
+
+class TPHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.render('tepper.html')
 
 if __name__ == '__main__':
 	tornado.options.parse_command_line()
