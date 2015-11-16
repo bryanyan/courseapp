cf1798dd582dfc5d6076143c0b2bd30ea6074c97
Bryan Yan <bryanyan@andrew.cmu.edu>
2015-11-15 23:53:05 -0500

    styling and more information on modules

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import os.path
+import io
+import json
+import pprint
 
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
 
-import cmu_course_api
-
-#Semester must be one of S (spring), M1 (Summer 1), M2 (Summer 2), or F (Fall).
-semester = 'S'
-# CMU AndrewID
-username = 'bryanyan'
-# Password 
-password = '*********'
-# Python Dictionary 
-# data = cmu_course_api.get_course_data(semester, username, password)
-
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
 
+def getCourses():
+	with open('static/courses.json') as dataFile:
+		data = json.load(dataFile)
+	return data['courses']
+
+data = getCourses()
+
 class Application(tornado.web.Application):
+
 	def __init__(self):
-		handlers = [(r'/', IndexHandler)]
+		handlers = [(r'/', IndexHandler),
+					(r'/scs', SCSHandler)]
 		settings = dict(template_path=os.path.join(os.path.dirname(__file__), "templates"),
 						static_path=os.path.join(os.path.dirname(__file__), "static"),
+						ui_modules={"Course": CourseModule},
 						debug=True)
 		tornado.web.Application.__init__(self, handlers, **settings)
 
@@ -31,29 +33,47 @@ class IndexHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render('index.html')
 
-class MCSHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render('mcs.html')
+#class MCSHandler(tornado.web.RequestHandler):
+#	def get(self):
+#		self.render('mcs.html')
 
 class SCSHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render('scs.html')
 
-class DCHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render('dc.html')
+	def getSCSCourses(self):
+		scsCourses = {}
+		for i in range(15050, 15720):
+			try:
+				course = data[str(i)]
+				scsCourses[str(i)] = course
+			except:
+				continue
+		scsCoursesList = scsCourses.items()
+		scsCoursesSorted = sorted(scsCoursesList,key=lambda x: x[0])
+		return scsCoursesSorted
 
-class CITHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.render('cit.html')
+		self.render('scs.html', courses=self.getSCSCourses())
 
-class CFAHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render('cfa.html')
+#class DCHandler(tornado.web.RequestHandler):
+#	def get(self):
+#		self.render('dc.html')
 
-class TPHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render('tepper.html')
+#class CITHandler(tornado.web.RequestHandler):
+#	def get(self):
+#		self.render('cit.html')
+
+#class CFAHandler(tornado.web.RequestHandler):
+#	def get(self):
+#		self.render('cfa.html')
+
+#class TPHandler(tornado.web.RequestHandler):
+#	def get(self):
+#		self.render('tepper.html')
+
+class CourseModule(tornado.web.UIModule):
+	def render(self, course):
+		return self.render_string("modules/courses.html", course=course)
+
 
 if __name__ == '__main__':
 	tornado.options.parse_command_line()
